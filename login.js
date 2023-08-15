8f0c64f137999f4d8b34864684bde4fcba2f1376
ahsan-ahmed-khan <142326094+ahsan-ahmed-khan@users.noreply.github.com>
2023-08-15 07:22:03 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    function login_success(){
+        window.alert("Login Success!");
+        window.location.href = "index.html";
+    }
+
+    username === "ahsan" && password == "ahsan123" ? login_success() : window.alert("Login Failed");
+
+
+};
